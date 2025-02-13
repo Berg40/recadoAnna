0c9a46f1fe491460536a7c2a998553c16c06ffa2
Berg Andrade <wandemberg28@gmail.com>
2025-02-13 16:29:01 -0300

    Versão1.2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -28,4 +30,5 @@ def recado5():
     return render_template('recado5.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
